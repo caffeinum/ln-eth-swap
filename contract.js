cd1cabb6a17976285fbc9e9dad681c18ff303e60
Aleksey Bykhun <theisease@gmail.com>
2018-06-23 16:57:04 +0200

    gas

--- contract.js
+++ contract.js
@@ -8,8 +8,7 @@ class Contract {
 
     this.contract = new web3.eth.Contract(abi, contract, {
       from: wallet.account.address,
-      gasPrice: '10000000000000',
-      gas: 1000000
+      // gas: 1e5
     })
   }
 
