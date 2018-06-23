5db1a0bede0327940a8aa2d47dd999ca0f7f87e6
Aleksey Bykhun <theisease@gmail.com>
2018-06-23 16:40:28 +0200

    pass args

--- contract.js
+++ contract.js
@@ -20,9 +20,9 @@ class Contract {
 
   async fund(hash) {
     // hash is Buffer
-    hash = new Uint8Array(hash).buffer
+    // hash = new Uint8Array(hash).buffer
     // hash should be bytes20
-    const receipt = await this.contract.methods.fund({ _secretHash: hash }).send()
+    const receipt = await this.contract.methods.fund(hash).send()
     return receipt
   }
 
